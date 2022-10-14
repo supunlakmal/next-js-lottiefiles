3232a08becf33f02cf954db17d78d6d892dec26a
Supun Lakmal Abesekara <supunlakmal61@gmail.com>
2022-10-14 15:39:37 +0000

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import { Player, Controls } from '@lottiefiles/react-lottie-player';
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <div className="flex flex-wrap">
   
 
 <Player
@@ -19,11 +19,41 @@ const Home: NextPage = () => {
 <Player
   autoplay
   loop
-  src="https://assets5.lottiefiles.com/packages/lf20_GhkD0k.json"
+  src="https://assets8.lottiefiles.com/packages/lf20_VSBhlA7ElW.json"
+  style={{ height: '300px', width: '300px' }}
+>
+</Player><Player
+  autoplay
+  loop
+  src="https://assets3.lottiefiles.com/packages/lf20_nTfkVR.json"
+  style={{ height: '300px', width: '300px' }}
+>
+</Player><Player
+  autoplay
+  loop
+  src="https://assets5.lottiefiles.com/datafiles/AtGF4p7zA8LpP2R/data.json"
+  style={{ height: '300px', width: '300px' }}
+>
+</Player><Player
+  autoplay
+  loop
+  src="https://assets5.lottiefiles.com/datafiles/HN7OcWNnoqje6iXIiZdWzKxvLIbfeCGTmvXmEm1h/data.json"
+  style={{ height: '300px', width: '300px' }}
+>
+</Player><Player
+  autoplay
+  loop
+  src="https://assets5.lottiefiles.com/packages/lf20_rycdh53q.json"
+  style={{ height: '300px', width: '300px' }}
+>
+</Player><Player
+  autoplay
+  loop
+  src="https://assets1.lottiefiles.com/datafiles/MUp3wlMDGtoK5FK/data.json"
   style={{ height: '300px', width: '300px' }}
 >
 </Player>
-   
+
     </div>
   )
 }

a06aee19cf1fb5be617df11f45535e16eeae2692
Supun Lakmal Abesekara <supunlakmal61@gmail.com>
2022-10-14 14:52:03 +0000

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,10 +12,10 @@ const Home: NextPage = () => {
   <Player
   autoplay
   loop
-  src="https://assets1.lottiefiles.com/packages/lf20_ybj0fjlo.json"
+  src="https://assets5.lottiefiles.com/packages/lf20_GhkD0k.json"
   style={{ height: '300px', width: '300px' }}
 >
-  <Controls visible={true} buttons={['play', 'repeat', 'frame', 'debug']} />
+  {/* <Controls visible={true} buttons={['play', 'repeat', 'frame', 'debug']} /> */}
 </Player>
 
    
